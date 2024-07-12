d60cb38b6bf01c68f65aceeed7ce4505d401413e
KIRAN KUMAR K <137183954+KIRAN-KUMAR-K3@users.noreply.github.com>
2024-07-12 15:25:54 +0530

    Delete student_course_project directory

--- student_course_project/courses/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StudentListView, StudentDetailView
-
-urlpatterns = [
-    path('students/', StudentListView.as_view(), name='student-list'),
-    path('students/<int:pk>/', StudentDetailView.as_view(), name='student-detail'),
-]
